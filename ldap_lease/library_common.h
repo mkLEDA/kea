bc636c190b13b4fbede0769a9508d7393036f8a2
Juliette Garreau <juliette@cri.epita.fr>
2018-07-09 19:08:46 +0200

    hooks: first try
    Signed-off-by: Juliette Garreau <juliette@cri.epita.fr>

---
+++ ldap_lease/library_common.h
@@ -0,0 +1,8 @@
+
+// library_common.h
+#ifndef LIBRARY_COMMON_H
+#define LIBRARY_COMMON_H
+#include <fstream>
+// "Interesting clients" log file handle declaration.
+extern std::fstream interesting;
+#endif // LIBRARY_COMMON_H
